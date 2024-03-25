2ec5da2912e60983079251870009db1a06643836
matttre3 <140153086+matttre3@users.noreply.github.com>
2024-03-25 23:18:26 +0100

    first commit

---
+++ personal-portfolio/src/App.tsx
@@ -0,0 +1,17 @@
+import './scss'
+
+function App() {
+
+
+  return (
+    <>
+      <section className='hero'>
+        <div className="container">
+
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
