680ac79c20c1ad06f3ce6eba216849cb8d723bb7
matttre3 <140153086+matttre3@users.noreply.github.com>
2024-03-27 23:25:41 +0100

    added animation on scroll

--- personal-portfolio/src/App.tsx
+++ personal-portfolio/src/App.tsx
@@ -1,11 +1,43 @@
 
 import Typewriting from "./components/Typewriting";
+import { useEffect } from "react";
+
 function App() {
+
+  useEffect(() => {
+    const menuBoxes = document.getElementsByClassName('box')
+    const elementsToAnimate = document.getElementsByClassName('dash-animate-me');
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('dash-animation');
+          observer.unobserve(entry.target); 
+        }
+      });
+    });
+
+    for (let i = 0; i < elementsToAnimate.length; i++) {
+      observer.observe(elementsToAnimate[i]);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []); 
 
 
   return (
     <>
       <section className="hero">
+
+        <div className="nav-boxes">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </div>
+
+
         <div className="container">
           <div className="heading">
             <h1>Hi👋, i'm 
@@ -22,7 +54,7 @@ function App() {
       <section className="projects">
         <div className="container">
          <div className="titleContainer">
-            <svg height="120" stroke='#212427' stroke-width="2" className="text-line" width="100%"><text x="50%" dominant-baseline="middle" text-anchor="middle" y="50%">PROJECTS</text></svg>
+            <svg height="120" stroke='#212427' stroke-width="2" className="text-line dash-animate-me" width="100%"><text x="50%" dominant-baseline="middle" text-anchor="middle" y="50%">PROJECTS</text></svg>
             <h2>PROJECTS</h2>
          </div> 
         </div>
